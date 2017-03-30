79845f910d8a71afb9b0558bdf905f6925af71db
kruehrig <kruehrig@student.tgm.ac.at>
2017-03-30 13:46:24 +0200

    5x guten Tag

--- src/ruehrig/Test0330.java
+++ src/ruehrig/Test0330.java
@@ -14,6 +14,10 @@ public class Test0330 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Guten Tag Hr. Rührig!!!!");
+		System.out.println("Guten Tag Hr. Rührig!!!!");
+		System.out.println("Guten Tag Hr. Rührig!!!!");
+		System.out.println("Guten Tag Hr. Rührig!!!!");
+		System.out.println("Guten Tag Hr. Rührig!!!!");
 	}
 
 }
