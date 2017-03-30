46302f9305a0b52bf798b1da2c6651544fd49777
kruehrig <kruehrig@student.tgm.ac.at>
2017-03-30 13:44:48 +0200

    Test0330 erstellt

---
+++ src/ruehrig/Test0330.java
@@ -0,0 +1,19 @@
+package ruehrig;
+
+/**
+ * Eine Klasse die einen String aus
+ * @author Kevin Rührig
+ * @version 3/30/2017
+ */
+public class Test0330 {
+
+	public Test0330() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Guten Tag Hr. Rührig!!!!");
+	}
+
+}
