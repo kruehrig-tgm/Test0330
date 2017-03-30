4b3d58c8bea7e51ee459fb835120c92c1e00dc9a
kruehrig <kruehrig@student.tgm.ac.at>
2017-03-30 13:48:58 +0200

    neuer Branch Good morning statt Guten Tag

--- src/ruehrig/Test0330.java
+++ src/ruehrig/Test0330.java
@@ -13,7 +13,7 @@ public class Test0330 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Guten Tag Hr. Rührig!!!!");
+		System.out.println("Good mornig Mr. Rührig!!!!");
 	}
 
 }
